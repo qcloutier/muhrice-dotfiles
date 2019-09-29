ddd03b9f88af42f152a19e84e82c7c85563d5360
qcloutier <quintoncloutier@outlook.com>
2019-09-29 18:07:40 -0300

    Delay before power operation

--- fish/.config/fish/functions/syspower.fish
+++ fish/.config/fish/functions/syspower.fish
@@ -3,6 +3,8 @@ function syspower
     if test (count $argv) = 0
         printf "Options:\n  '-s' to suspend\n  '-h' to hibernate\n  '-r' to reboot\n  '-p' to poweroff\n"
     else
+        sleep 3s
+
         switch $argv[1]
             case '-s'
                 sudo systemctl suspend
