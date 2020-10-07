826e86b70998845e98cd9f2b06e9b8129ed15ecb
Quinton Cloutier <quintoncloutier@outlook.com>
2020-10-07 11:34:46 -0300

    Percentage on battery

--- yabar/.config/yabar/battery.sh
+++ yabar/.config/yabar/battery.sh
@@ -4,14 +4,15 @@ BATS="$(echo /sys/class/power_supply/BAT*)"
 test "$BATS" = '/sys/class/power_supply/BAT*' && exit
 
 for BAT in $BATS; do
-	if [ "$(cat $BAT/capacity)" -le 20 ]; then
-		printf '❗'
+	CAP="$(cat $BAT/capacity)"
+	if [ "$CAP" -le 20 ]; then
+		printf "❗ $CAP%%"
 	else
 		case "$(cat $BAT/status)" in
-			'Charging')    printf '🔌' ;;
-			'Discharging') printf '🔋' ;;
-			'Full')        printf '⚡' ;;
-			*)             printf '❔' ;;
+			'Charging')    printf "🔌 $CAP%%" ;;
+			'Discharging') printf "🔋 $CAP%%" ;;
+			'Full')        printf "⚡ $CAP%%" ;;
+			*)             printf "❔ $CAP%%" ;;
 		esac
 	fi
 done
