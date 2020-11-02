48d456715a68693f47fff8ecdc6fa406cfe62230
Quinton Cloutier <quintoncloutier@outlook.com>
2020-11-01 23:06:13 -0400

    Switch from bspwm to i3

--- polybar/.config/polybar/window.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if ! command -v bspc xprop >/dev/null; then
-	exit 0
-fi
-
-TITLE="$(xprop -id "$(bspc query -n focused -N)" 2> /dev/null)"
-printf "$TITLE" | grep 'WM_CLASS' | awk -F \" '{print $4}'
