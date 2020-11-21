855cfff00d83de5b6eb4f08b49951040aec6297b
Quinton Cloutier <quintoncloutier@outlook.com>
2020-11-21 18:25:48 -0400

    Firefox userjs and userchrome

---
+++ firefox/user.js
@@ -0,0 +1,16 @@
+// Behaviour
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("middlemouse.contentLoadURL", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+// Performance
+user_pref("gfx.webrender.all", false);
+user_pref("layers.acceleration.force-enabled", true);
+
+// Theming
+user_pref("browser.uidensity", 1);
+user_pref("devtools.theme", "dark");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("widget.content.allow-gtk-dark-theme", true);
