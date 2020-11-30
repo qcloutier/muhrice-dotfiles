35e2382cf5aad0cc1944e95021b2d8b4eaa0c3aa
Quinton Cloutier <quintoncloutier@outlook.com>
2020-11-30 19:54:24 -0400

    Hardware video acceleration in Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -3,12 +3,16 @@ user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.bookmarks.showMobileBookmarks", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("extensions.pocket.enabled", false);
-user_pref("middlemouse.contentLoadURL", true);
 user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("middlemouse.contentLoadURL", true);
 
 // Performance
 user_pref("gfx.webrender.all", true);
 user_pref("layers.acceleration.force-enabled", true);
+user_pref("media.av1.enabled", false);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
 
 // Theming
 user_pref("browser.uidensity", 1);
